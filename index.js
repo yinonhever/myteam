0d726b530c76183308ae323ab8f0cc43904a9725
yinonhever <yinonehever@gmail.com>
2020-06-15 17:02:05 +0300

    first commit - adding header and mobile navigation

---
+++ index.js
@@ -0,0 +1,38 @@
+// MANAGING THE MOBILE MENU
+
+$(".header__nav-toggle").click(function () {
+    $(".header__navigation").addClass("active");
+    $("body").addClass("no-scroll");
+})
+
+$(".header__nav-close").click(function () {
+    $(".header__navigation").removeClass("active");
+    $("body").removeClass("no-scroll");
+})
+
+if ($(window).width() <= 600) {
+    animateMenuItems();
+}
+
+$(window).resize(function () {
+    if ($(window).width() <= 600) {
+        animateMenuItems();
+    }
+
+    else {
+        $(".header__nav-link, .header__btn").css("animation", "none");
+    }
+})
+
+function animateMenuItems() {
+    let delay = 0.2;
+
+    $(".header__nav-link").each(function () {
+        $(this).css("animation", "slideUp 0.5s " + delay + "s");
+        delay = delay + 0.2;
+    })
+
+    $(".header__btn").css("animation", "slideUp 0.5s " + delay + "s");
+
+    $(".header__nav-link, .header__btn").css("animation-fill-mode", "backwards");
+}
